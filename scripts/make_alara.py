df19c1739d3c6e0be9fe84e3ed38c146919e74a0
ajaber <ajaber@2cba87ba-dc11-4c1a-8d40-db79849a50ef>
2011-11-01 21:24:28 +0000

    Added handling to deal with void mixtures. The script will now not create new mixtures for elements that are void. It will also assign those zones with void in the mat_loading.

--- scripts/make_alara.py
+++ scripts/make_alara.py
@@ -1,14 +1,19 @@
 #! /usr/bin/env python
 
-# mmGridGen to ALARA
-# Written by: Amir Jaber
-# 10/27/2011
+# The input to this script is the output from mmGridGen's meshReader.
+# This script writes a file with the ALARA input for the geometry and mixing 
+# definition (volume, mat_loading, and mixture definitions). 
+# This user must specfiy the path to the input file and path of the output file
+# on the command line. The program will prompt for the user to enter the total 
+# volume of the geometry  
 
-# Open file and read input
-# inputfile = raw_input('Enter the input file name: ')
-# input = open(inputfile, 'r')
-input = open('matFracs_results.txt', 'r')
+# Written by: Amir Jaber 
+
+import sys
+# Open input file, read input, and create output file
+input = open(sys.argv[1], 'r')
 readinp = input.readlines()
+alara_input = open(sys.argv[2], 'w')	
 
 # Determine order of material IDs
 matID = readinp[0].strip().split()
@@ -18,21 +23,21 @@ for i in range(0,len(matID)):
 nummats=len(matID)
 print 'Materials order: ', matID
 
-# Determine dimensions of mesh 
+# Determine intervals of mesh 
 dimID = readinp[1].strip().split()
 del dimID[0]
 for i in range(0,len(dimID)):
 	dimID[i]=int(dimID[i])
-print 'Number of xyz dimensions: ', dimID
+print 'Number of xyz intervals: ', dimID
 
-# Calculate volume per mesh element
+# Calculate volume per mesh element assuming equal volume for all elements
 totalvolume = float(raw_input('Enter the volume: '))
 numzones=(dimID[0]*dimID[1]*dimID[2])
 elementvolume = totalvolume/numzones
 print 'Volume per mesh element: ', elementvolume
 
-# Create file to write ALARA input into
-alara_input = open('alara_input', 'w')
+# Write ALARA geometry card to file
+alara_input.write('geometry rectangular\n\n')
 
 # Write ALARA volume card to file
 alara_input.write('volume\n')
@@ -44,19 +49,27 @@ alara_input.write('end\n\n')
 # Write ALARA mat_loading card to file
 alara_input.write('mat_loading\n')
 for i in range(1,numzones+1):
-	matname='\t'+'zone_'+str(i)+'\t'+'mix_'+str(i)+'\n'
-      	alara_input.write(matname)
+	zonemats=readinp[i+1].strip().split()
+	if float(zonemats[0])==1.0:
+		matname='\t'+'zone_'+str(i)+'\t'+'void'+'\n'
+      		alara_input.write(matname)	
+	else:
+		matname='\t'+'zone_'+str(i)+'\t'+'mix_'+str(i)+'\n'
+      		alara_input.write(matname)
 alara_input.write('end\n\n')
 
 # Write ALARA mixture definitions to file
 for i in range(1,numzones+1):
-	mixname='mixture'+'\t'+'mix_'+str(i)+'\n'
-	alara_input.write(mixname)
-	for j in range(1,nummats):
-		zonemats=readinp[i+1].strip().split()
-		mixdef='\t'+'mat_'+str(matID[j])+'\t'+str(float(zonemats[j]))+'\n'
-		alara_input.write(mixdef)
-	alara_input.write('end\n\n')
+	zonemats=readinp[i+1].strip().split()
+	if float(zonemats[0])==1.0:
+		continue
+	else:
+		mixname='mixture'+'\t'+'mix_'+str(i)+'\n'
+		alara_input.write(mixname)
+		for j in range(1,nummats):
+			mixdef='\t'+'mat_'+str(matID[j])+'\t'+str(float(zonemats[j]))+'\n'
+			alara_input.write(mixdef)
+		alara_input.write('end\n\n')
 
 
 
