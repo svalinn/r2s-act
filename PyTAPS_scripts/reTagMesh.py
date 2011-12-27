d96236e6660cdb3dae1157d8ba4169040b5474fc
ajaber <ajaber@2cba87ba-dc11-4c1a-8d40-db79849a50ef>
2011-12-27 00:33:57 +0000

    Added meshtal reader. Creates tag for each energy group and the total and stores each on a mesh.

--- PyTAPS_scripts/reTagMesh.py
+++ PyTAPS_scripts/reTagMesh.py
@@ -7,11 +7,16 @@ from itaps import iBase,iMesh
 from optparse import OptionParser
 import sys
 
-def reTagMesh( mesh ):
+def TagMats( mesh ):
+    print "Tagging mesh with material volume fractions"
     count=0
     mats=mesh.getTagHandle("MATS")
+    dims=mesh.getTagHandle("GRID_DIMS")
     fracs=mesh.getTagHandle("FRACTIONS")
     matID=list(mats[mesh.rootSet])
+    print "\tMaterials order: "+str(matID)
+    dimID=list(dims[mesh.rootSet])
+    print "\tNumber of xyz intervals: "+str(dimID)+'\n'
     voxels = mesh.getEntities(iBase.Type.region)
 
     for num in matID :
@@ -25,19 +30,53 @@ def reTagMesh( mesh ):
         count=count+1
     return 
 
+def TagFluxes( mesh, meshtal ) :
+    print "Tagging mesh with neutron fluxes"
+    Input=open(meshtal, "r")
+    InputLines=Input.readlines() 
+    TableHeading ='   Energy         '
+    n=1 #number of lines in mcnp file header
+    HeaderLine=InputLines[0][:64]
+    while HeaderLine != TableHeading:
+          n=n+1
+          HeaderLine=InputLines[n][:18]
+    print '\tSkipping Header:', n, 'lines'
+    m=n+1 #first line of values
+    j=1
+    while InputLines[j+m-1][:11] == InputLines[j+m][:11]:
+          j=j+1
+    print '\tMesh points found:', j
+    k=(len(InputLines)-j-m)/j #number of energy groups
+    print '\tEnergy bins found:', k
+    voxels = mesh.getEntities(iBase.Type.region)
+    column=[]; count=0 ; m=m-1
+    for groupID in range(1,k+2):
+        tag=mesh.createTag("group_"+str(groupID),1,float)
+        for x in range(1,j+1):
+            num = InputLines[m+x+j*count]
+            column.append(num[42:53])
+        count=count+1
+        tag[voxels]=column
+        column=[]
+    return 
+
 def parser():
     parser = OptionParser(usage="usage: %prog <in> [options]")
     parser.set_defaults(output='matFracs.vtk')
     parser.add_option("-o", "--output",
                       action="store", dest="output",
                       help="output filename (can be .h5m or .vtk)")
-   
+    parser.add_option("-m", "--meshtal",
+                      action="store", dest="meshtal",
+                      help="meshtal file name to be store on mesh")   
     (options, args) = parser.parse_args()
-    return options.output
+    return (options, args)
   
 if __name__=='__main__':
-     out = parser()
+     (options, args)= parser()
      mesh = iMesh.Mesh()
      mesh.load( sys.argv[1] )
-     reTagMesh( mesh )
-     mesh.save( out )
+     TagMats( mesh )
+     if options.meshtal != None :
+         TagFluxes ( mesh, options.meshtal )
+     mesh.save( options.output )
