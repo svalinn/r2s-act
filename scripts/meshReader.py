0c36ececf004883f0694c775e8b4eaa093495ed6
biondo <biondo@2cba87ba-dc11-4c1a-8d40-db79849a50ef>
2012-01-19 19:30:11 +0000

    changed folder name to begin with test_ for nosetests, forgot to svn add moved files from previous commit

---
+++ scripts/meshReader.py
@@ -0,0 +1,104 @@
+#! /usr/bin/env python
+
+# The input to this script is a mesh file output from mmGridGen.
+# This script writes a file with the ALARA input for the geometry and mixing 
+# definition (volume, mat_loading, and mixture definitions). 
+# This user must specfiy the path to the input file, path of the output file
+# on the command line, and the total volume of the geometry.  
+
+# Written by: Amir Jaber
+
+from itaps import iBase,iMesh
+import re
+import sys
+
+def meshReader(filename):
+    nmfile = open("matFracs_results", 'w')
+    mesh = iMesh.Mesh()
+    mesh.load( filename )
+
+    fracs=mesh.getTagHandle("FRACTIONS")
+    errors=mesh.getTagHandle("ERRORS")
+    mats=mesh.getTagHandle("MATS")  
+    dims=mesh.getTagHandle("GRID_DIMS")
+    
+    nmfile.write("Materials "+re.sub("[\[\]\,]","",str(mats[mesh.rootSet]))+"\n")
+    nmfile.write("Dimensions "+re.sub("[\[\]\,]","",str(dims[mesh.rootSet]))+"\n")
+    for i in fracs[mesh.getEntities(iBase.Type.region)] :
+        nmfile.write(re.sub("[\[\]\,]","",str(map(lambda x: round(x ,6), list(i))))+"\n")
+    nmfile.close()
+    return 
+  
+def make_alara(output_filename, geom_volume):
+     # Open input file, read input, and create output file
+     input = open("matFracs_results", 'r')
+     readinp = input.readlines()
+     alara_input = open(output_filename, 'w')
+     
+     # Determine order of material IDs
+     matID = readinp[0].strip().split()
+     del matID[0]
+     matID=map(int ,matID)
+     nummats=len(matID)
+     print 'Materials order: ', matID
+
+     # Determine intervals of mesh 
+     dimID = readinp[1].strip().split()
+     del dimID[0]
+     dimID=map(int ,dimID)
+     print 'Number of xyz intervals: ', dimID
+     
+     # Calculate volume per mesh element assuming equal volume for all elements
+     totalvolume = geom_volume
+     try : 
+          float(totalvolume)
+     except :
+          print >>sys.stderr, "Invalid entry for volume."
+          sys.exit(1)
+     numzones=(dimID[0]*dimID[1]*dimID[2])
+     elementvolume = float(totalvolume)/numzones
+     print 'Volume per mesh element: ', elementvolume
+     
+     # Write ALARA geometry card to file
+     alara_input.write('geometry rectangular\n\n')
+
+     # Write ALARA volume card to file
+     alara_input.write('volume\n')
+     for i in range(1,numzones+1):
+         zonename='\t'+str(elementvolume)+'\t'+'zone_'+str(i)+'\n'
+         alara_input.write(zonename)
+     alara_input.write('end\n\n')
+     
+     # Write ALARA mat_loading card to file
+     alara_input.write('mat_loading\n')
+     for i in range(1,numzones+1):
+         zonemats=readinp[i+1].strip().split()
+         if float(zonemats[0])==1.0:
+             matname='\t'+'zone_'+str(i)+'\t'+'void'+'\n'
+             alara_input.write(matname)  
+         else:
+             matname='\t'+'zone_'+str(i)+'\t'+'mix_'+str(i)+'\n'
+             alara_input.write(matname)
+     alara_input.write('end\n\n')
+     
+     # Write ALARA mixture definitions to file
+     for i in range(1,numzones+1):
+         zonemats=readinp[i+1].strip().split()
+         if float(zonemats[0])==1.0:
+             continue
+         else:
+            mixname='mixture'+'\t'+'mix_'+str(i)+'\n'
+            alara_input.write(mixname)
+            for j in range(1,nummats):
+                     mixdef='\tmaterial\t'+'mat_'+str(matID[j])+'\t'+\
+                     str(1)+'\t'+str(float(zonemats[j]))+'\n'
+                     alara_input.write(mixdef)
+            alara_input.write('end\n\n')
+     input.close()
+     alara_input.close()
+     return
+
+if __name__=='__main__':
+     meshReader(sys.argv[1])
+     make_alara(sys.argv[2],sys.argv[3])
+	
---
+++ scripts/reTagMesh.py
@@ -0,0 +1,250 @@
+#! /usr/bin/env python
+from itaps import iBase,iMesh
+from optparse import OptionParser
+import re
+import sys
+
+def parser():
+    parser = OptionParser(usage="usage: %prog <in> [options]")
+    parser.add_option("-m", "--meshtal",
+                      action="store", dest="meshtal",
+                      help="Path to meshtal input file")
+    parser.add_option("-n", "--normalization", dest="Norm", type="float",
+                      default=1, help="Flux normalization factor, default: %default")
+    parser.add_option("-v", "--volume",
+                      action="store", dest="volume", default="1",
+                      help="Total volume of the geometry, default: %default")
+    parser.add_option("-o", "--output",
+                      action="store", dest="output", default="mesh.vtk",
+                      help="Mesh output filename (can be .h5m or .vtk), default: %default")
+    parser.add_option("-f", "--fluxinoutput",
+                      action="store", dest="fluxin", default="ALARAflux.in",
+                      help="Name of the ALARA flux input file, default: %default")    
+    parser.add_option("-a", "--alara",
+                      action="store", dest="alaraname", default="ALARA.in",
+                      help="Name of the ALARA input file, default: %default")
+    parser.add_option('-b', action='store_true', dest='backwardbool', 
+                      default=False, help="print fluxes in decreasing energy")    
+    
+    return parser.parse_args()
+
+def FindFirstLine(MeshtalInputLines):#Finding # of lines to skip
+    TableHeading ='   Energy         '
+    n=1 #number of lines in mcnp file header
+    HeaderLine=MeshtalInputLines[0][:64]
+    while HeaderLine != TableHeading:
+          n=n+1
+          HeaderLine=MeshtalInputLines[n][:18]
+    print 'Skipping Header:', n, 'lines'
+    m=n+1 #first line of values
+    return m
+
+def MeshPointCount(MeshtalInputLines, m):#Counting Number of Meshpoints
+    j=1 #initialising # of points
+    while MeshtalInputLines[j+m-1][:11] == MeshtalInputLines[j+m][:11]:
+          j=j+1
+    print 'Mesh points found:', j
+    return j
+
+def EnergyGroupCount(MeshtalInputLines, m, j):#finding number of energy groups
+    k=(len(MeshtalInputLines)-j-m)/j #number of energy groups, note, the "-j" is used to neglect flux "Total" rows in
+    print 'Energy bins found:', k
+    return k
+
+def PrintLowtoHigh(MeshtalInputLines, m, j, k, Norm, fluxin):#Printing values to fluxin file
+    FluxinOutput=file(fluxin, "w")
+    for t in range(0, j):
+        pointoutput=''
+        for s in range(0,k):
+           pointoutput+=str(float(MeshtalInputLines[m+s*j + t][42:53])*Norm)+' '
+           if (s+1)%8 == 0 & s != (k-1):
+               pointoutput+='\n'
+        FluxinOutput.write(pointoutput + '\n\n')
+    print 'File creation sucessful \n'
+
+def PrintHightoLow(MeshtalInputLines, m, j, k, Norm, fluxin):#Printing values to fluxin file
+    FluxinOutput=file(fluxin, "w")
+    for t in range(0, j):
+        pointoutput=''
+        for s in range(k-1,-1,-1):
+           pointoutput+=str(float(MeshtalInputLines[m+s*j + t][42:53])*Norm)+' '
+           if (s+1)%8 == 0 & s != (k-1):
+               pointoutput+='\n'
+        FluxinOutput.write(pointoutput + '\n\n')
+    print 'ALARAflux.in file creation sucessful \n'
+
+def TagMats(mesh):
+    print 'Tagging mesh with material volume fractions'
+    count=0
+    mats=mesh.getTagHandle("MATS")
+    dims=mesh.getTagHandle("GRID_DIMS")
+    fracs=mesh.getTagHandle("FRACTIONS")
+    matID=list(mats[mesh.rootSet])
+    print '\tMaterials order: '+str(matID)
+    dimID=list(dims[mesh.rootSet])
+    print '\tNumber of xyz intervals: '+str(dimID)+'\n'
+    voxels = mesh.getEntities(iBase.Type.region)
+
+    for num in matID :
+        column=[]
+        tag=mesh.createTag('mat_'+str(num),1,float)
+        
+        for i in fracs[voxels] :
+            column.append(i[count])    
+        tag[voxels]=column
+        count=count+1
+    return
+
+def TagFluxes(mesh, meshtal, m, j, k) :
+    print 'Tagging mesh with neutron fluxes'
+    voxels = mesh.getEntities(iBase.Type.region)
+    column=[]; count=0 ;
+    for n_groupID in range(1,k+2):
+        tag=mesh.createTag("n_group_"+str(n_groupID),1,float)
+        for x in range(0,j):
+            num = MeshtalInputLines[m+x+j*count]
+            column.append(num[42:53])
+        count=count+1
+        tag[voxels]=column
+        column=[]
+    return
+
+def CloseFiles(): #Closes meshtal input and flux.in output files
+    MeshtalInput.close()
+    FluxinOutput.close()
+
+def meshReader( filename ):
+    nmfile = open("matFracs_results", 'w')
+    mesh = iMesh.Mesh()
+    mesh.load( filename )
+
+    fracs=mesh.getTagHandle("FRACTIONS")
+    errors=mesh.getTagHandle("ERRORS")
+    mats=mesh.getTagHandle("MATS")  
+    dims=mesh.getTagHandle("GRID_DIMS")
+    
+    nmfile.write("Materials "+re.sub("[\[\]\,]","",str(mats[mesh.rootSet]))+"\n")
+    nmfile.write("Dimensions "+re.sub("[\[\]\,]","",str(dims[mesh.rootSet]))+"\n")
+    for i in fracs[mesh.getEntities(iBase.Type.region)] :
+        nmfile.write(re.sub("[\[\]\,]","",str(map(lambda x: round(x ,6), list(i))))+"\n")
+    nmfile.close()
+    return 
+  
+def make_alara(output_filename, geom_volume):
+     # Open input file, read input, and create output file
+     input = open("matFracs_results", 'r')
+     readinp = input.readlines()
+     alara_input = open(output_filename, 'w')
+     
+     # Determine order of material IDs
+     matID = readinp[0].strip().split()
+     del matID[0]
+     matID=map(int ,matID)
+     nummats=len(matID)
+     print 'Materials order: ', matID
+
+     # Determine intervals of mesh 
+     dimID = readinp[1].strip().split()
+     del dimID[0]
+     dimID=map(int ,dimID)
+     print 'Number of xyz intervals: ', dimID
+     
+     # Calculate volume per mesh element assuming equal volume for all elements
+     totalvolume = geom_volume
+     numzones=(dimID[0]*dimID[1]*dimID[2])
+     elementvolume = float(totalvolume)/numzones
+     print 'Volume per mesh element: ', elementvolume
+     
+     # Write ALARA geometry card to file
+     alara_input.write('geometry rectangular\n\n')
+
+     # Write ALARA volume card to file
+     alara_input.write('volume\n')
+     for i in range(1,numzones+1):
+         zonename='\t'+str(elementvolume)+'\t'+'zone_'+str(i)+'\n'
+         alara_input.write(zonename)
+     alara_input.write('end\n\n')
+     
+     # Write ALARA mat_loading card to file
+     alara_input.write('mat_loading\n')
+     for i in range(1,numzones+1):
+         zonemats=readinp[i+1].strip().split()
+         if float(zonemats[0])==1.0:
+             matname='\t'+'zone_'+str(i)+'\t'+'void'+'\n'
+             alara_input.write(matname)  
+         else:
+             matname='\t'+'zone_'+str(i)+'\t'+'mix_'+str(i)+'\n'
+             alara_input.write(matname)
+     alara_input.write('end\n\n')
+     
+     # Write ALARA mixture definitions to file
+     for i in range(1,numzones+1):
+         zonemats=readinp[i+1].strip().split()
+         if float(zonemats[0])==1.0:
+             continue
+         else:
+            mixname='mixture'+'\t'+'mix_'+str(i)+'\n'
+            alara_input.write(mixname)
+            for j in range(1,nummats):
+                     mixdef='\tmaterial\t'+'mat_'+str(matID[j])+'\t'+\
+                     str(1)+'\t'+str(float(zonemats[j]))+'\n'
+                     alara_input.write(mixdef)
+            alara_input.write('end\n\n')
+     input.close()
+     alara_input.close()
+     return
+
+def check_input(norm, vol):
+    try:
+        float(norm)
+    except:
+        print >>sys.stderr, "Invalid entry for normalization factor"
+        sys.exit(1)
+    try : 
+        float(vol)
+    except :
+        print >>sys.stderr, "Invalid entry for volume."
+        sys.exit(1)
+
+def check_meshpoints(MeshtalInputLines, m, j, k, mesh):
+    voxels = mesh.getEntities(iBase.Type.region)
+    if j !=len(voxels):
+        print >>sys.stderr, 'Number of meshtal points does not match number of mesh points'
+        sys.exit(1)
+
+    #find number of data points in InputLines
+    l=0 #number of datapoints
+    while MeshtalInputLines[m+l][:11] != '   Total   ':
+        l=l+1
+    print 'total data points found:', l
+    if l != (j*k) :
+        print >>sys.stderr, 'Number of data points does not equal meshpoints*energy groups'
+        sys.exit(1)
+   
+if __name__=='__main__':
+    (options, args)= parser()
+    mesh = iMesh.Mesh()
+    mesh.load(args[0])
+    TagMats(mesh)    
+    if options.meshtal != None :
+        check_input(options.Norm, options.volume)
+        print 'Parsing meshtal file'
+        MeshtalInput=open(options.meshtal, "r")
+        MeshtalInputLines=MeshtalInput.readlines() 
+        FluxinOutput=file(options.fluxin, "w")
+        m=FindFirstLine(MeshtalInputLines)
+        j=MeshPointCount(MeshtalInputLines, m)
+        k=EnergyGroupCount(MeshtalInputLines, m,j)
+        check_meshpoints(MeshtalInputLines, m, j, k, mesh)
+        if options.backwardbool==False:
+            PrintLowtoHigh(MeshtalInputLines, m, j, k, options.Norm, options.fluxin)
+        else:
+            PrintHightoLow(MeshtalInputLines, m, j, k, options.Norm, options.fluxin)
+        TagFluxes (mesh, options.meshtal, m, j, k)
+        mesh.save(options.output)
+        CloseFiles()
+    print "generating ALARA input"
+    meshReader(args[0])
+    make_alara(options.alaraname,options.volume)
+    print '\ncomplete'
+	
