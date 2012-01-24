e6ece7d366cc6fa6fbbcf6a3a4179de9bcde2fb6
biondo <biondo@2cba87ba-dc11-4c1a-8d40-db79849a50ef>
2012-01-24 23:18:42 +0000

    FluxParse: added the ability to tag fluxes and errors onto user defined mesh

--- scripts/FluxParse.py
+++ scripts/FluxParse.py
@@ -5,6 +5,7 @@
 import linecache
 from optparse import OptionParser
 import sys
+from itaps import iMesh, iBase
 
 #use for lines in filename
 def find_meshtal_type(meshtal):
@@ -47,13 +48,13 @@ def meshtal_to_array(meshtal, m):
     while line[:11] != '   Total   ' :
         column_count=0
         char_count=0
-        column_data=[0]*5
-        while column_count < 5 :
+        column_data=[0]*6
+        while column_count <= 5:
             if line[char_count] == ' ':
                 char_count = char_count +1
             else :
-                min_char =char_count
-                while line[char_count] != ' ':
+                min_char = char_count
+                while line[char_count] != ' ' and char_count<len(line)-1:
                     char_count=char_count +1
                 max_char=char_count
                 column_data[column_count]=float(line[min_char:max_char])
@@ -65,7 +66,7 @@ def meshtal_to_array(meshtal, m):
 
 def count_mesh_points(array) :
     j=1
-    while abs(array[j][0]/array[0][0] - 1) < 0.0001 :
+    while abs(array[j][0]/array[0][0] - 1) < 0.00001 :
         j=j+1
     print 'Mesh points found:', j
     return j
@@ -126,12 +127,40 @@ def print_fluxin(array, j, k, norm, output_file, backwardbool):
                pointoutput+='\n'
         output.write(pointoutput + '\n\n')
     print 'ALARA flux input file,', output_file , ', created sucessfully'
-     
+
+def tag_fluxes_preexisting(array, j, k, mestal_type, mesh_input, mesh_output):
+    mesh = iMesh.Mesh()
+    mesh.load(mesh_input)
+    voxels = mesh.getEntities(iBase.Type.region)
+    column_flux=[]
+    column_error=[]
+    count=0    
+    for groupID in range(1,k+1): #need to add one for total fluxes
+        tag_flux=mesh.createTag(meshtal_type+'_group_'+str(groupID),1,float)
+        tag_error=mesh.createTag(meshtal_type+'_group_'+str(groupID)+'_error',1,float)
+        for x in range(0,j):
+            array_index=x+j*count            
+            column_flux.append(array[array_index][4])
+            column_error.append(array[array_index][5])
+        count=count+1
+        tag_flux[voxels]=column_flux
+        tag_error[voxels]=column_error
+        column_flux=[]
+        column_error=[]
+        mesh.save(mesh_output)
+    if meshtal_type == 'n':
+        print 'Tagged user supplied mesh with neutron fluxes'
+    if meshtal_type == 'p':
+        print 'Tagged user supplied mesh with photon fluxes'
+   
 if __name__=='__main__':
     print 'Parsing meshtal file'
     parser = OptionParser()
-    parser.add_option('-b', action='store_true', dest='backwardbool', default=False, help="Print fluxes in decreasing energy")
-    parser.add_option('-o', dest='output_file', default='ALARAflux.in', help="Name of output file")
+    parser.add_option('-b', action='store_true', dest='backward_bool', default=False, help='Print to ALARA fluxin in fluxes in decreasing energy')
+    parser.add_option('-o', dest='fluxin_name', default='ALARAflux.in', help='Name of ALARA fluxin output file')
+    parser.add_option('-m',  dest='mesh_input', default= 'none', help = 'Tag meshes onto user preexisting mesh, supply file name')
+    parser.add_option('-p', dest='mesh_output', default='mesh.vtk', help = 'Name of mesh output file')
+    parser.add_option('-s', action='store_true', dest='supress_mesh', default=False, help='Supress creation of mesh file, only fluxin file is created')
     (opts, args) = parser.parse_args()
     meshtal_type=find_meshtal_type(args[0])
     m=find_first_line(args[0])
@@ -142,6 +171,10 @@ if __name__=='__main__':
     x,y,z=count_delinations(array)
     check_meshtal_data(l, j, k, x, y, z)
     norm=float(args[1])
-    print_fluxin(array, j, k, norm, opts.output_file, opts.backwardbool)
+    print_fluxin(array, j, k, norm, opts.fluxin_name, opts.backward_bool)
+    #if opts.mesh_input = 'none' and opts.supress_mesh == 'False'
+    
+    if opts.mesh_input != 'none':
+        tag_fluxes_preexisting(array, j, k, meshtal_type, opts.mesh_input, opts.mesh_output)
 
 
