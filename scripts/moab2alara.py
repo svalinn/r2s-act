356881406c8bb61ad82ab79520c540d4c68a8536
ajaber <ajaber@2cba87ba-dc11-4c1a-8d40-db79849a50ef>
2012-02-02 20:18:21 +0000

    Takes information directly from MOAB mesh and write alara input. This currently prints the same information as make_alara.py but does so directly from the .h5m file rather than from the output of the meshReader.py intermediate. New features (such as no repeated mixtures) are to be implemented next

---
+++ scripts/moab2alara.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python
+
+# The input to this script is the MOAB file (h5m) from mmGridGen.
+
+# Written by: Amir Jaber
+
+from itaps import iBase,iMesh
+from optparse import OptionParser
+
+def parser():
+    parser = OptionParser(usage="usage: %prog <in> [options]")
+    parser.add_option("-o", "--output",
+                      action="store", dest="output", default="alara_geom",
+                      help="Name of generated ALARA input file, default: %default")
+    return parser.parse_args()
+
+def moab2alara(mesh, filename):
+    # Write ALARA geometry card to file
+    filename.write('geometry rectangular\n\n')
+    
+    mats = mesh.getTagHandle("MATS")
+    dims = mesh.getTagHandle("GRID_DIMS")
+    fracs = mesh.getTagHandle("FRACTIONS")
+    matID = list(mats[mesh.rootSet])
+    
+    # To be redone later to allow for coarse/fine meshes
+    dimID = list(dims[mesh.rootSet])
+    numzones=(dimID[0]*dimID[1]*dimID[2])
+    elementvolume = float(1)/numzones
+    
+    voxels = mesh.getEntities(iBase.Type.region)
+#    filename.write(str(fracs[voxels]))
+
+# Write ALARA volume card to file
+    filename.write('volume\n')
+    for i in range(1,len(voxels)) :
+         zonename='\t'+str(elementvolume)+'\t'+'zone_'+str(i)+'\n'
+         filename.write(zonename)
+    filename.write('end\n\n')
+
+# Write ALARA mat_loading card to file
+    filename.write('mat_loading\n')
+    for i in range(1,len(voxels)) :
+         zonemats=list(fracs[voxels][i])
+         if float(zonemats[0])==1.0:
+             matname='\t'+'zone_'+str(i)+'\t'+'void'+'\n'
+             filename.write(matname)  
+         else:
+             matname='\t'+'zone_'+str(i)+'\t'+'mix_'+str(i)+'\n'
+             filename.write(matname)
+    filename.write('end\n\n')
+     
+# Write ALARA mixture definitions to file
+    nummats=len(matID)
+    for i in range(1,len(voxels)) :
+        if float(zonemats[0])==1.0 :
+            continue
+        else :
+            mixname='mixture'+'\t'+'mix_'+str(i)+'\n'
+            filename.write(mixname)
+            for j in range(1,nummats):
+                     mixdef='\tmaterial\t'+'mat_'+str(matID[j])+'\t'+\
+                     str(1)+'\t'+str(float(zonemats[j]))+'\n'
+                     filename.write(mixdef)
+            filename.write('end\n\n')
+    filename.close()
+
+    return
+        
+def main() :
+    (options, args)= parser()
+    alara_input = open(options.output, 'w')
+
+    mesh = iMesh.Mesh()
+    print "Read "+args[0]
+    mesh.load(args[0])
+    moab2alara(mesh, alara_input)  
+    print "Wrote "+options.output
+
+if __name__=='__main__':
+    main()
