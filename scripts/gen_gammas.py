d03ce1e90a365c8f67af98bd45b253dd329ddc0c
relson <relson@2cba87ba-dc11-4c1a-8d40-db79849a50ef>
2012-03-07 18:51:21 +0000

    Reworked code to use the new scdmesh capabilities.  gen_gammas takes the photon source strength tags from the H5M mesh, normalizes them (now with variable voxel size normalization), and creates the gammas file.  Currently there is a mismatch between new and old gammas files, and this is being investigated.

---
+++ scripts/gen_gammas.py
@@ -0,0 +1,324 @@
+#!/usr/bin/env python 
+
+######################################################################
+#gen_gammas.py
+######################################################################
+# This python script reads the photon source strength tags from a
+#  structured MOAB mesh, normalizes these values, and the creates the
+#  'gammas' file to be used with a custom source.F90 routine in MCNP.
+#
+#
+#
+######################################################################
+
+import textwrap as tw
+import os
+from optparse import OptionParser
+from itaps import iBase,iMesh
+import alias
+from scdmesh import ScdMesh, ScdMeshError
+
+
+def gen_gammas_file_from_h5m(inputfile, outfile="gammas"):
+    """Generate gammas file using information from tags on a MOAB mesh.
+    
+    ACTION: Method reads tags with photon source strengths from an h5m
+    file and generates the gammas file for the modified KIT source.f90 routine
+    To do this, we generate meshprobs and call gen_gammas_file().
+    REQUIRES: the .h5m moab mesh must have photon source strength tags of the
+    form "phtn_src_group_###"
+    Will read photon energy bin boundary values if the root set has the tag 
+    PHOT_ERG (a list of floats)
+
+    RECEIVES: The file (a moab mesh file) containing the mesh of interest.
+    An output file name for the 'gammas' file.
+    TODO:
+    """
+
+    mesh = iMesh.Mesh()
+
+    # Create ScdMesh object, which also loads inputfile into mesh.
+    sm = ScdMesh.fromFile(mesh, inputfile)
+
+    try:
+        grouptag = mesh.getTagHandle("phtn_src_group_001")
+    except iBase.TagNotFoundError:
+        print "ERROR: The file '{0}' does not contain tags of the " \
+                "form 'phtn_src_group_#'".format(inputfile)
+        return 0
+
+    voxels = mesh.getEntities(iBase.Type.region)
+    numvoxels = len(voxels)
+
+    #Need to create: meshstrengths, self.meshprobs
+    meshprobs = list() # of lists of strings; each string is a source strength
+    meshstrengths = list() # of floats; each float is the total source
+                                #  strength of a voxel at the chosen cooling step
+
+    # Initialize list to first erg bin source strength value for each voxel
+    meshstrengths = [float(x) for x in grouptag[voxels]]
+
+    numergbins = 0
+
+    # We now go through all photon energy groups and sum the individual bins
+    #  to get the total source strength in each voxel
+    for i in xrange(2,1000): #~ Arbitrary: we look for up to 1000 groups
+        try:
+            grouptag = mesh.getTagHandle("phtn_src_group_{0:03d}".format(i))
+            for cnt, vox in enumerate(voxels):
+                meshstrengths[cnt] += float(grouptag[vox])
+        except iBase.TagNotFoundError: 
+            numergbins = i - 1
+            break
+    
+    print "Found tags for for {0} photon energy bins.".format(numergbins)
+
+    vols = calc_volumes_list(sm)
+
+    if len(vols) != len(meshstrengths):
+        print "ERROR: mismatch in calculated number of volumes and number of " \
+                "voxel source strengths."
+        return 0
+
+    # We calcualte the normalization factor as the sum over all voxels of
+    #  voxel source strength * voxel volume
+    # Divided by the volume of all voxels with non-zero source strength.
+    # This applies for variabl voxel sizes in a structured mesh.
+    numactivatedcells = 0
+    sumvoxelstrengths = 0
+    sourcevolumetotal = 0
+    for cnt, meshstr in enumerate(meshstrengths):
+        if meshstr > 0:
+            numactivatedcells += 1
+            sumvoxelstrengths += vols[cnt] * meshstr
+            sourcevolumetotal += vols[cnt]
+    print "The number of activated voxels and total number of voxels is " \
+            "{0}/{1}".format(numactivatedcells, len(meshstrengths))
+    
+    norm = sumvoxelstrengths / sourcevolumetotal
+
+    # We now look for the tags with the energy bin boundary values
+    try:
+        phtn_ergs = mesh.getTagHandle("PHTN_BINS")
+        myergbins = phtn_ergs[mesh.rootSet] #UNTESTED... does this need list() around it?
+        print "NOTE: photon energy bins were found in the .h5m mesh and added to " \
+                "'{0}'. The modified mcnp5/source.f90 looks for a file caled " \
+                "'gammas_ener'".format(outfile)
+    except: # if there is no PHTN_BINS tag, then we send an empty string in its place
+        myergbins = ""
+
+    fw = _gen_gammas_header(sm, outfile, myergbins)
+
+    for voxel in voxels:
+        writestring = ""
+        binval = 0.0
+        for i in xrange(1, numergbins + 1):
+            binval += float(mesh.getTagHandle( \
+                    "phtn_src_group_{0:03d}".format(i))[voxel])
+            writestring += "{0:<12.5E}".format(binval/norm)
+        fw.write(writestring + "\n")
+    
+    fw.close()
+
+    print "The file '{0}' was created successfully".format(outfile)
+
+    return 1
+
+
+def _gen_gammas_header(scd, outfile, ergbins):
+    """Open a stream to write the header information for a gammas file
+    
+    ACTION: Method writes the header lines for gammas file, and method
+    is used by both gen_gammas_file_from_h5m() and gen_gammas_file_aliasing().
+    RECEIVES: scd is an scdmesh.ScdMesh object.
+    RETURNS: A file writing object, fw.
+    """
+    
+    # calculate the mesh spacing in each direction
+
+    fw = open(outfile, 'w')
+    
+    # write 1st line (intervals for x, y, z)
+    extents = [scd.dims[3], scd.dims[4], scd.dims[5]]
+    fw.write(" ".join([str(x) for x in extents]) + "\n")
+    
+    # create and write x mesh edges line (2nd line)
+    fw.write(" ".join([str(x) for x in scd.getDivisions('x')]) + "\n")
+    # create and write y mesh edges line (3rd line)
+    fw.write(" ".join([str(y) for y in scd.getDivisions('y')]) + "\n")
+    # create and write z mesh edges line (4th line)
+    fw.write(" ".join([str(z) for z in scd.getDivisions('z')]) + "\n")
+
+    # create and write 5th line (list of activated materials... ?????)
+    fw.write(" ".join([str(x) for x in xrange(1,101)]) + "\n")
+
+    # If storing the energy bins information, write line 6
+    if ergbins != "":
+        fw.write(" ".join([str(x) for x in ergbins]) + "\n")
+
+    return fw
+
+
+#def gen_gammas_file_aliasing(meshform, outfile="gammas_alias", ergbins=""):
+#    """Generate the gammas_alias file for alias table version of source.F90
+#    
+#    ACTION: Method creates the file gammas with the photon energy bins
+#    for each voxel stored as alias tables. Reads directly from phtn_src file.
+#    Header information is the same as that in gen_gammas_file().
+#
+#    Each voxel's line corresponds with an alias table of the form:
+#    [total source strength, p1, g1a, g1b, p2, g2a, g2b ... pN, gNa, gNb]
+#    Where each p#, g#a, g#b are the info for one bin in the alias table.
+#
+#    REQUIRES: Method assumes that read() and isotope_source_strengths() 
+#    have been called already, OR that self.meshstrengths and self.meshprobs
+#    have been otherwise created properly.
+#    RECEIVES:
+#    3D list, meshform, of the form {{xmin,xmax,xintervals},{y...},{z...}}
+#    Optional: 'outfile' allows for an alternate file name instead of gammas
+#    Optional: 'ergbins' can specify different energy bins, rather than a
+#       default set of 42 bins. (This adds an additional line in gammas
+#       and requires a different source.F90.)
+#    RETURNS: 1 if successful, 0 if an error occurred.
+#    """
+#
+#    try:
+#        nmesh = len(self.meshstrengths) # We use this to check for a warning
+#                        # and to make sure a required method has been called
+#    except:
+#        print "ERROR: isotope_source_strengths needs to be called before " \
+#                "gen_sdef_probabilties"
+#        return 0
+#
+#    if nmesh != meshform[0][2]*meshform[1][2]*meshform[2][2]:
+#        print "WARNING: Number of mesh cells in phtn_src file does not " \
+#                "match the product of the mesh intervals given:"
+#        print "     ", nmesh, "!=", \
+#                meshform[0][2],"*",meshform[1][2],"*",meshform[2][2]
+#
+#    # We call the function that opens our gammas file and writes the header
+#    fw = self._gen_gammas_header(meshform, outfile, ergbins)
+#
+#    # We calcualte the normalization factor as the average total source
+#    #  strength in each mesh cell divided by the number of mesh cells
+#    #  with non-zero source strength.
+#    # This applies ONLY for uniform voxel size meshing.
+#    numactivatedcells = 0
+#    for val in self.meshstrengths:
+#        if val > 0:
+#            numactivatedcells += 1 
+#            # volsourcetot += volcell
+#    print "The number of activated voxels and total number of voxels is " \
+#            "{0}/{1}".format(numactivatedcells, nmesh)
+#    norm = sum(self.meshstrengths) / numactivatedcells
+#
+#    for binset in self.meshprobs:
+#
+#        # We create the list to send to alias.gen_alias_table()
+#        ergproblist = list()
+#        sourcetotal = sum([float(prob) for prob in binset]) # binset)
+#
+#        # Special case of a zero source strength voxel...
+#        # In this case, we write the correct number of zeros to the gammas
+#        #  file so that the source.F90 code doesn't run out of values to read
+#        if sourcetotal == 0:
+#            fw.write(" ".join([0]*(len(binset)*3+1)) + "\n")
+#            continue
+#
+#        for cnt, p in enumerate(binset):
+#            ergproblist.append([float(p)/sourcetotal, cnt+1])
+#
+#        aliastable = alias.gen_alias_table(ergproblist)
+#
+#        # We compactly format the alias table as a bunch of strings.
+#        # The following list comprehension uses a format specifying output
+#        #  of 12 characters, with 5 values after the decimal point and 
+#        #  scientific notation for the probability values, and integer 
+#        #  format for the energy group numbers
+#        aliasstrings = ["{0:<12.5E} {1} {2}".format(x[0][0],x[0][1],x[1][1]) \
+#                for x in aliastable]
+#
+#        fw.write(str(sourcetotal/norm) + " " + " ".join(aliasstrings) + "\n")
+#
+#    # all lines written, close file.
+#    fw.close()
+#    
+#    print "The file '{0}' was created successfully".format(outfile)
+#
+#    return 1
+
+
+def calc_volumes_list(scd):
+    """Create list of voxel volumes for a cartesian mesh
+    
+    ACTION: Method creates a 1D list of voxel volumes
+    RECEIVES: scd, a scdmesh.ScdMesh object.
+    RETURNS: vols, a 1D list of voxel volumes in order 'xyz'
+
+    NOTE: Voxel ordering follows meshtal file convention which is
+     -for x for y iterate z
+     -for x iterate y
+     -iterate x
+    """
+    
+    meshplanes = [ scd.getDivisions('x'), \
+                    scd.getDivisions('y'), \
+                    scd.getDivisions('z') ]
+
+    oldz = meshplanes[2][0]
+    oldy = meshplanes[1][0]
+    oldx = meshplanes[0][0]
+
+    nxdiv = len(meshplanes[0]) - 1
+    nydiv = len(meshplanes[1]) - 1
+    nzdiv = len(meshplanes[2]) - 1
+
+    # create 1D list
+    vols = [0] * (nxdiv * nydiv * nzdiv)
+
+    for cntx, x in enumerate(meshplanes[0][1:]):
+        oldy = meshplanes[1][0]
+        for cnty, y in enumerate(meshplanes[1][1:]):
+            oldz = meshplanes[2][0]
+            for cntz, z in enumerate(meshplanes[2][1:]):
+                # Calc volume here
+                vols[nydiv*nzdiv*cntx + nzdiv*cnty + cntz] = \
+                        (x - oldx) * \
+                        (y - oldy) * \
+                        (z - oldz)
+                oldz = z
+            oldy = y
+        oldx = x
+    print vols
+    return vols
+
+
+def main():
+    """ACTION: Method defines an option parser and handles command-line
+    usage of this module.
+    REQUIRES: command line arguments to be passed - otherwise prints help
+    information.
+    RECEIVES: N/A
+    """
+
+    usage = "usage: %prog input-h5m-file [options] arg"
+    parser = OptionParser(usage)
+    
+    # Input and mesh file names
+    parser.add_option("-o","--output",action="store",dest="output", \
+            default="gammas",help="Option specifies the name of the 'gammas'" \
+            "file. Default: %default")
+
+    (options, args) = parser.parse_args()
+
+    print options.output
+    print args
+    gen_gammas_file_from_h5m( args[0], options.output )
+
+    return 1
+
+
+# Handles module being called as a script.
+if __name__=='__main__':
+    main()
+
