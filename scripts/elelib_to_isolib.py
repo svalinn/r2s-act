e047744292a0cb067e66db2481f9f1f5c80193f5
biondo <biondo@2cba87ba-dc11-4c1a-8d40-db79849a50ef>
2012-04-02 01:31:03 +0000

    deleted line_to_list and modified elelib_to_isolib to use .split()

--- scripts/elelib_to_isolib.py
+++ scripts/elelib_to_isolib.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python
 import linecache
-import line_to_list
 from optparse import OptionParser
 
 def create_elelib(inp, outp):
     output=file(outp, 'w')
     line_index=1
     line=linecache.getline(inp, line_index)
-    while line !='':
-        line_list=line_to_list.convert(line, (1,2,3,4,5), ('str','str','str','str','str'), (' ','\t'))
-        if line_list[4] !=0:
-             num_isos=int(line_list[4])
 
+    while line !='':
+
+        line_list=line.split()
+        if len(line_list)==5:
+             num_isos=int(line_list[4])
+            
              for x in range(0,5):
                  output.write(line_list[x]+' ')
              output.write('\n')
+
              for y in range(1,num_isos+1):
                  output.write('\t\t\t')
                  for z in range(0,2):
-                     output.write(line_to_list.convert(linecache.getline(inp,line_index+y), (1,2),('str','str'),(' ','\t'))[z]+' ')
+                     output.write(linecache.getline(inp,line_index+y).split()[z]+' ')
                  output.write('\n')                 
+             output.write('\n')
 
              for iso_index in range (1,num_isos+1):
-                 iso_line=line_to_list.convert(linecache.getline(inp,line_index+iso_index), (1,2), ('str','str'), (' ','\t'))
+                 iso_line=linecache.getline(inp,line_index+iso_index).split()
                  output.write(line_list[0]+':'+iso_line[0]+' '+line_list[1]+' '+\
                      line_list[2]+' '+line_list[3]+' '+ '1\n\t\t\t'+iso_line[0]+'\t100'+'\n')
+                 output.write('\n')
         line_index=line_index+1
         line=linecache.getline(inp, line_index)
 def main( arguments = None ):
