dd3429e77c3f12f4fa7e85b37b6049f58c8bf505
relson <relson@2cba87ba-dc11-4c1a-8d40-db79849a50ef>
2012-04-04 20:09:20 +0000

    Added two tests for ModMCNPforPhotons.py

---
+++ scripts/testing/test_ModMCNPforPhotons.py
@@ -0,0 +1,44 @@
+import unittest
+import ModMCNPforPhotons
+import os.path
+import itertools
+
+# These directories are relative to scripts directory.
+testfile = "testing/inp_ModMCNPforPhotons"
+testfile_out = "testing/inp_ModMCNPforPhotons_p"
+testfile_compare = "testing/inp_ModMCNPforPhotons_converted"
+
+
+class TestRegularMCNP(unittest.TestCase):
+
+    def setUp(self):
+        self.obj = ModMCNPforPhotons.ModMCNPforPhotons(testfile, dagmc=False)
+        self.obj.read()
+        self.obj.change_block_1()
+        self.obj.change_block_2()
+        self.obj.change_block_3()
+
+    def tearDown(self):
+        os.system("rm " + testfile_out)
+
+    def test_run(self):
+        """
+        Tests successful run-through
+        """
+        
+        self.assertEqual(self.obj.write_deck(testfile_out), 1)
+
+    def test_consistent(self):
+        """
+        """
+
+        self.obj.write_deck(testfile_out)
+        fw1 = open(testfile_out, 'r')
+        fw2 = open(testfile_compare, 'r')
+   
+        for line1, line2 in itertools.izip_longest(fw1, fw2):
+            self.assertEqual(line1, line2)
+
+        fw1.close()
+        fw2.close()
+
