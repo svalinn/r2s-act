42fe8e9db1bc4303be622569cb6414b29e5cfcf1
relson <relson@2cba87ba-dc11-4c1a-8d40-db79849a50ef>
2012-06-29 17:55:03 +0000

    Added photon_by_voxel parameter to the default r2s.cfg file text that is located in r2s_setup.py. This is necessary for writing the correct gammas file for the mcnp5_voxel executable.

--- scripts/r2s_setup.py
+++ scripts/r2s_setup.py
@@ -107,15 +107,19 @@ mmgrid_rays = 10
 # -photon_cooling is the cooling step read from phtn_src. 0 is shutdown,
 #  other numbers correspond with cooling times listed in your ALARA input.
 # -if photon_bias is 1 (true), the gammas file will try to include voxel bias
-#  values from the mesh (stored as PHTN_BIAS tag)
+#  values from the mesh (stored as PHTN_BIAS tag). Currently requires
+#  photon_by_voxel to be 1.
 # -if alias_ergbins is 1 (true), photon energy bin distribution for each voxel
 #  is stored in alias table format. This increases size of gammas, but makes
 #  MCNP calculations slightly faster.
+# -if photon_by_voxel is 1 (true), gammas file is written for voxel sampling
+#  version of source.F90.
 photon_isotope = TOTAL
 photon_cooling = 0
 # Next two use 0 for false, 1 for true
 photon_bias = 0
 alias_ergbins = 1
+photon_by_voxel = 1
 
 [r2s-material]
 # This section may be used to map material idenfitiers to material names.
