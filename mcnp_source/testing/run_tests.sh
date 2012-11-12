41a25978ff21a607accb2229cb69a89d596dddfe
Eric Relson <gertlex@gmail.com>
2012-11-11 20:18:36 -0600

    Compiling test with debugging enabled.

--- mcnp_source/testing/run_tests.sh
+++ mcnp_source/testing/run_tests.sh
@@ -2,11 +2,11 @@
 python hide_source.py ../source_gamma_refactor.F90
 
 # Generate the .o files..
-gfortran -c mcnp_placeholder.F90
-gfortran -c source.F90
+gfortran -c mcnp_placeholder.F90 -g
+gfortran -c source.F90 -g
 
 # compile!
-gfortran test_source.F90 -o test_source   mcnp_placeholder.o source.o
+gfortran test_source.F90 -o test_source   mcnp_placeholder.o source.o -g
 
 # run test
 ./test_source
