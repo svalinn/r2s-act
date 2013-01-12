fa464d079908a6a072aa11f5b35b5d5f4f7d87ff
Eric Relson <gertlex@gmail.com>
2013-01-11 18:06:52 -0600

    Fulfilling conf.py needs for the uwtheme.

--- docs/conf.py
+++ docs/conf.py
@@ -98,17 +98,20 @@ html_theme = 'uwtheme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+'site_title' : u'UW-R2S',
+'tagline' : u'3-D Neutron Activation Workflow'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'UW-R2S - CNERG'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'UW-R2S'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -122,11 +125,11 @@ html_theme_path = ['_themes']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../_static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -247,6 +250,7 @@ texinfo_documents = [
 sys.path.insert(0, '../scripts/r2s/')
 sys.path.insert(0, '../scripts/')
 
+# Include __init__() docstrings when using sphinx-apidoc?
 def skip(app, what, name, obj, skip, options):
     if name == "__init__":
         return False
