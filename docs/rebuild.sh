ba1a5a1bb17104e148054c204fcdd5061a0091d1
Eric Relson <gertlex@gmail.com>
2013-01-18 01:00:13 -0600

    Added source_gamma.F90 to auto-documented code. WIP.

--- docs/rebuild.sh
+++ docs/rebuild.sh
@@ -1,6 +1,10 @@
 # This is mean to be run from r2s-act/, NOT from r2s-act/docs/
 # See docs/README.rst
 
+# create fake list of functions and docstrings from source_gamma.F90
+mkdir scripts/r2s/mcnp/
+cat \# > scripts/r2s/mcnp/__init__.py
+python docs/gen_source_gamma_doc.py mcnp_source/source_gamma.F90 scripts/r2s/mcnp/source_gamma_doc.py
 # regenerate the .rst files for modules in scripts/r2s/ and below
 sphinx-apidoc -f -o docs/r2s scripts/r2s
 # then make html as usual
