b580ba5fe8fdc2b85593dda21e4a37beb511441c
Eric Relson <gertlex@gmail.com>
2013-01-18 11:47:27 -0600

    Refactored step 2; testing is WIP.

---
+++ scripts/r2s/testing/test_r2s_step2.py
@@ -0,0 +1,43 @@
+import unittest
+import sys
+import os
+import os.path
+from tempfile import NamedTemporaryFile as NTF
+from tempfile import mkdtemp
+from shutil import rmtree
+import contextlib
+
+import r2s_step2 as s2
+
+class TestLoadConfig(unittest.TestCase):
+    #
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_load_config_files(self):
+        """
+        """
+        pass
+
+    def test_load_config_params(self):
+        """
+        """
+        pass
+
+
+
+class TestHandlePhtnData(unittest.TestCase):
+    #
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testHandlePhtnData(self):
+        pass
--- scripts/r2s_step2.py
+++ scripts/r2s_step2.py
@@ -11,103 +11,135 @@ from itaps import iMesh, iMeshExtensions
 from r2s_setup import get_input_file, FileMissingError
 
 
-cfgfile = 'r2s.cfg'
-if len(sys.argv) > 1:
-    cfgfile = sys.argv[1]
+##################
+# Read config file
+def load_config_files(config):
+    """Read in config file information for files
+    """
 
-config = ConfigParser.SafeConfigParser()
-config.read(cfgfile)
+    # Required input files
+    if config.has_option('r2s-files', 'step2_datafile'):
+        datafile = get_input_file(config, 'step2_datafile')
+    else:
+        datafile = get_input_file(config, 'step1_datafile')
 
-
-###########################
-# Read in config file information
-
-# Required input files
-if config.has_option('r2s-files', 'step2_datafile'):
-    datafile = get_input_file(config, 'step2_datafile')
-else:
-    datafile = get_input_file(config, 'step1_datafile')
-
-phtn_src = get_input_file(config, 'alara_phtn_src')
+    phtn_src = get_input_file(config, 'alara_phtn_src')
 
 
-# Filenames
-mcnp_p_problem = None
-if config.has_option('r2s-files','photon_mcnp_input'):
-    mcnp_n_problem = config.get('r2s-files','neutron_mcnp_input')
-    mcnp_p_problem = config.get('r2s-files','photon_mcnp_input')
-    
+    # Filenames
+    mcnp_p_problem = None
+    if config.has_option('r2s-files','photon_mcnp_input'):
+        mcnp_n_problem = config.get('r2s-files','neutron_mcnp_input')
+        mcnp_p_problem = config.get('r2s-files','photon_mcnp_input')
+        
+    return (datafile, phtn_src, mcnp_n_problem, mcnp_p_problem)
 
-# Read other parameters from r2s.cfg
+def load_config_params(config):
+    """Read in config file information for parameters
+    """
 
-# This list stores (1) parameter names as listed in r2s.cfg; (2) their defaults;
-#  (3) which 'get' function to use for the parameter
-param_guide = [ #parameter  #default #get function
-        [ 'photon_isotope', "TOTAL", config.get],
-        [ 'photon_cooling', 0,       config.get],
-        [ 'sampling'      , 'v',     config.get],
-        [ 'custom_ergbins', False,   config.getboolean],
-        [ 'photon_bias'   , False,   config.getboolean],
-        [ 'cumulative'    , False,   config.getboolean],
-        [ 'add_fmesh_card', True,    config.getboolean]
-        ] 
+    # This list stores (1) parameter names as listed in r2s.cfg; 
+    # (2) their defaults; (3) which 'get' function to use for the parameter
+    param_guide = [ #parameter  #default #get function
+            [ 'photon_isotope', "TOTAL", config.get],
+            [ 'photon_cooling', 0,       config.get],
+            [ 'sampling'      , 'v',     config.get],
+            [ 'custom_ergbins', False,   config.getboolean],
+            [ 'photon_bias'   , False,   config.getboolean],
+            [ 'cumulative'    , False,   config.getboolean],
+            [ 'add_fmesh_card', True,    config.getboolean]
+            ] 
 
-param_list = list()
+    param_list = list()
 
-for param in param_guide:
-    try:
-        # Try to read from r2s.cfg
-        # Note that param[2] is a function
-        param_list.append( param[2]('r2s-params', param[0]) )
-    except ConfigParser.NoOptionError:
-        # Use default
-        param_list.append( param[1])
+    for param in param_guide:
+        try:
+            # Try to read from r2s.cfg
+            # Note that param[2] is a function
+            param_list.append( param[2]('r2s-params', param[0]) )
+        except ConfigParser.NoOptionError:
+            # Use default
+            param_list.append( param[1])
 
-(opt_isotope, opt_cooling, opt_sampling, opt_ergs, opt_bias, opt_cumulative, opt_phtnfmesh) = param_list
+    (opt_isotope, opt_cooling, opt_sampling, opt_ergs, opt_bias, \
+            opt_cumulative, opt_phtnfmesh) = param_list
+
+    return (opt_isotope, opt_cooling, opt_sampling, opt_ergs, opt_bias, opt_cumulative, opt_phtnfmesh)
 
 
 ###########################
 # Do step 2
+def handle_phtn_data(datafile, phtn_src, opt_isotope, opt_cooling,  \
+        opt_sampling, opt_bias, opt_cumulative, cust_ergbins):
+    """Loads phtn_src data, tags this to mesh, and generates 'gammas' file.
+    """
+    print "Loading step one data file '{0}'".format(datafile)
+    smesh = ScdMesh.fromFile(datafile)
 
-print "Loading step one data file '{0}'".format(datafile)
-smesh = ScdMesh.fromFile(datafile)
+    # Tagging mesh
+    print "Reading ALARA photon source '{0}'".format(phtn_src)
+    read_alara_phtn.read_to_h5m(phtn_src, smesh, isotope=opt_isotope, \
+            coolingstep=opt_cooling, retag=True, totals=True)
 
-# Tagging mesh
-print "Reading ALARA photon source '{0}'".format(phtn_src)
-read_alara_phtn.read_to_h5m(phtn_src, smesh, isotope=opt_isotope, \
-        coolingstep=opt_cooling, retag=True, totals=True)
+    print "Saving photon source information to '{0}'".format(datafile)
+    smesh.imesh.save(datafile)
 
-print "Saving photon source information to '{0}'".format(datafile)
-smesh.imesh.save(datafile)
+    with open(phtn_src, 'r') as fr:
+        coolingstepstring = read_alara_phtn.get_cooling_step_name( \
+                opt_cooling, fr)[0]
 
-fr = open(phtn_src, 'r')
-coolingstepstring = read_alara_phtn.get_cooling_step_name(opt_cooling, fr)[0]
-fr.close()
+    print "Writing gammas file"
+    write_gammas.gen_gammas_file_from_h5m(smesh, sampling=opt_sampling, \
+            do_bias=opt_bias, cumulative=opt_cumulative, \
+            cust_ergbins=opt_ergs, coolingstep=coolingstepstring, \
+            isotope=opt_isotope)
 
-print "Writing gammas file"
-write_gammas.gen_gammas_file_from_h5m(smesh, sampling=opt_sampling, \
-        do_bias=opt_bias, cumulative=opt_cumulative, cust_ergbins=opt_ergs, \
-        coolingstep=coolingstepstring, isotope=opt_isotope)
+    return smesh
 
-# Create photon MCNP input file from neutron input if it doesn't exist already
-if mcnp_p_problem:
 
-    if os.path.exists(mcnp_p_problem):
-        print "MCNP photon transport input file '{0}' already exists and will" \
-                " not be recreated.".format(mcnp_p_problem)
+def gen_mcnp_p(smesh, mcnp_p_problem, mcnp_n_problem, opt_phtnfmesh):
+    """Create photon MCNP input file from neutron input if it doesn't exist
+    already
+    """
+    if mcnp_p_problem:
 
-    else:
-        print "Modifying MCNP input file '{0}'".format(mcnp_n_problem)
+        if os.path.exists(mcnp_p_problem):
+            print "MCNP photon transport input file '{0}' already exists " \
+                    "and will not be recreated.".format(mcnp_p_problem)
 
-        mod = mcnp_n2p.ModMCNPforPhotons(mcnp_n_problem)
-        mod.read()
-        mod.convert()
+        else:
+            print "Modifying MCNP input file '{0}'".format(mcnp_n_problem)
 
-        # If phtnfmesh is True, we generate an fmesh card with same 
-        #  layout as found on the scdmesh.
-        if opt_phtnfmesh:
-            mod.add_fmesh_from_scdmesh(smesh)
+            mod = mcnp_n2p.ModMCNPforPhotons(mcnp_n_problem)
+            mod.read()
+            mod.convert()
 
-        mod.write_deck(mcnp_p_problem)
+            # If phtnfmesh is True, we generate an fmesh card with same 
+            #  layout as found on the scdmesh.
+            if opt_phtnfmesh:
+                mod.add_fmesh_from_scdmesh(smesh)
 
-print "" # Empty line to separate output from multiple runs of this script
+            mod.write_deck(mcnp_p_problem)
+
+    return
+
+
+if __name__ == "__main__":
+
+    cfgfile = 'r2s.cfg'
+    if len(sys.argv) > 1:
+        cfgfile = sys.argv[1]
+
+    config = ConfigParser.SafeConfigParser()
+    config.read(cfgfile)
+
+    (datafile, phtn_src, mcnp_n_problem, mcnp_p_problem) = load_config_files(config)
+
+    (opt_isotope, opt_cooling, opt_sampling, opt_ergs, opt_bias, opt_cumulative, opt_phtnfmesh) = load_config_params(config)
+
+    smesh = handle_phtn_data(datafile, phtn_src, opt_isotope, opt_cooling, \
+            opt_sampling, opt_bias, opt_cumulative, opt_ergs)
+
+    gen_mcnp_p(smesh, mcnp_p_problem, mcnp_n_problem, opt_phtnfmesh)
+
+    print "" # Empty line to separate output from multiple runs of this script
