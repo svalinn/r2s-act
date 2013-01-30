ef5edd6db3402acf54357c05ecb1fff75012afa1
Eric Relson <gertlex@gmail.com>
2013-01-30 13:54:05 -0600

    Script's path correction for running MCNP

--- mcnp_source/testing/test_problem_3/run_slab_tests.sh
+++ mcnp_source/testing/test_problem_3/run_slab_tests.sh
@@ -1,13 +1,13 @@
 cd resample_slab_uv
 rm out? runtp? meshta? -f
-~/DAG-MCNP/5.1.51/trunk/Source/src/mcnp5 i=mcnp.inp
+../../../mcnp5p i=mcnp.inp
 cd ..
 cd resample_slab_ua
 rm out? runtp? meshta? -f
-~/DAG-MCNP/5.1.51/trunk/Source/src/mcnp5 i=mcnp.inp
+../../../mcnp5p i=mcnp.inp
 cd ..
 cd resample_slab_vv
 rm out? runtp? meshta? -f
-~/DAG-MCNP/5.1.51/trunk/Source/src/mcnp5 i=mcnp.inp
+../../../mcnp5p i=mcnp.inp
 cd ..
 gvim -p resample_slab_uv/meshtal resample_slab_ua/meshtal resample_slab_vv/meshtal
