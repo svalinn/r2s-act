04fffd1664ffc7013360245f5e99ab8f093ce853
Elliott D. Biondo <biondo@tux-35.cae.wisc.edu>
2013-02-10 00:32:44 -0600

    added tests for wwinp_to_h5m.py, fixed bug in mats2ALARA test

--- scripts/tools/testing/test_mats2ALARA.py
+++ scripts/tools/testing/test_mats2ALARA.py
@@ -2,45 +2,46 @@ from tools import mats2ALARA
 from nose.tools import assert_equal
 import os
 
-inp = 'mcnp_inp.txt'
-output = 'alara_matlib.txt'
+def test_mats2ALARA():
+    inp = 'mats2ALARA_test_inp.txt'
+    output = 'alara_matlib.txt'
+    
+    if output in os.listdir('.'):
+        os.remove(output)
+    
+    mats2ALARA.mats_to_alara(inp, output)
+    
+    with open(output) as f:
+        written = f.read()
 
-if output in os.listdir('.'):
+    expected =(
+    '# mat number: 1\n'
+    '# source:  Some http//URL.com\n'
+    '# comments:  first line of comments second comments third line of comments fort\n'
+    '# h line of comments\n'
+    ' leu 19.1 2\n'
+    '     u:235 4.0000E-02 92\n'
+    '     u:238 9.6000E-01 92\n'
+    '# mat number: 2\n'
+    '# source:  internet\n'
+    '# comments:  Here are comments the comments continue here are more even more\n'
+    ' water 0.9 2\n'
+    '     h 1.1190E-01 1\n'
+    '     o 8.8810E-01 8\n'
+    '# mat number: 2\n'
+    '# source:  internet\n'
+    '# comments:  Here are comments the comments continue here are more even more\n'
+    ' water 1.00215363272 2\n'
+    '     h 1.1190E-01 1\n'
+    '     o 8.8810E-01 8\n'
+    'pseudo_void 0 1\n'
+    'he 1 2'
+    )
+
+    assert_equal(written, expected)
     os.remove(output)
-
-mats2ALARA.mats_to_alara(inp, output)
-
-with open(output) as f:
-    written = f.read()
-
-expected =(
-'# mat number: 1\n'
-'# source:  Some http//URL.com\n'
-'# comments:  first line of comments second comments third line of comments fort\n'
-'# h line of comments\n'
-' leu 19.1 2\n'
-'     u:235 4.0000E-02 92\n'
-'     u:238 9.6000E-01 92\n'
-'# mat number: 2\n'
-'# source:  internet\n'
-'# comments:  Here are comments the comments continue here are more even more\n'
-' water 0.9 2\n'
-'     h 1.1190E-01 1\n'
-'     o 8.8810E-01 8\n'
-'# mat number: 2\n'
-'# source:  internet\n'
-'# comments:  Here are comments the comments continue here are more even more\n'
-' water 1.00215363272 2\n'
-'     h 1.1190E-01 1\n'
-'     o 8.8810E-01 8\n'
-'pseudo_void 0 1\n'
-'he 1 2'
-)
-
-assert_equal(written, expected)
-os.remove(output)
 
 # Run as script
 #
 if __name__ == "__main__":
-    nose.main()
+    test_mats2ALARA()
---
+++ scripts/tools/testing/test_wwinp_to_h5m.py
@@ -0,0 +1,38 @@
+from tools import wwinp_to_h5m
+from nose.tools import assert_equal
+import os
+from itaps import iMesh, iBase
+from r2s.scdmesh import ScdMesh
+
+def test_wwinp_to_h5m():
+    wwinp = 'wwinp_test.txt'
+    output = 'wwinp_mesh.h5m'
+    particle = 'n'
+    
+    if output in os.listdir('.'):
+        os.remove(output)
+    
+    wwinp_to_h5m.cartesian(wwinp, output, particle)
+    
+    with open(output) as f:
+        written = f.read()
+    
+    expected_sm = ScdMesh.fromFile('expected_wwinp_mesh.h5m')
+    written_sm = ScdMesh.fromFile(output)
+    
+    for x in range(0,5):
+        for y in range(0,6):
+            for z in range(0,10):
+                expected_voxel = expected_sm.getHex(x,y,z)
+                expected = expected_sm.imesh.getTagHandle('ww_n_group_001')[expected_voxel]
+                written_voxel = written_sm.getHex(x,y,z)
+                written = written_sm.imesh.getTagHandle('ww_n_group_001')[written_voxel]
+                assert_equal(written, expected)
+    
+    os.remove(output)
+
+
+# Run as script
+#
+if __name__ == "__main__":
+    test_wwinp_to_h5m()
