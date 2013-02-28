7b02a9a9ba1816c865de4c7da2a15e0b636bfdc0
Eric Relson <gertlex@gmail.com>
2013-02-28 17:32:23 -0600

    Format/style cleanup.

--- scripts/r2s/io/write_alara_fluxin.py
+++ scripts/r2s/io/write_alara_fluxin.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
+
 import linecache
 from optparse import OptionParser
 import sys
 from itaps import iMesh, iBase
 from r2s.scdmesh import ScdMesh
 
+
 def find_num_e_groups(sm):
-    
-    num_e_groups=0
+    """ """    
+    num_e_groups = 0
     for e_group in range(1,1000): #search for up to 1000 e_groups
         
         #Look for tags in the form n_group_'e_group'
         try:
             tag=sm.imesh.getTagHandle('n_group_{0:03d}'.format(e_group))
-            num_e_groups = num_e_groups + 1 #increment if tag is found
+            num_e_groups += 1 #increment if tag is found
 
        # Stop iterating once a tag is not found
         except iBase.TagNotFoundError:
             break
-    
 
-    if num_e_groups != 0 :
+    if num_e_groups != 0:
         print 'Energy groups found: {0}'.format(num_e_groups)
 
     #Exit if no tags of the form n_group_XXX are found
@@ -30,33 +31,35 @@ def find_num_e_groups(sm):
   
     return num_e_groups
 
-def print_fluxes(sm, num_e_groups, backward_bool, fluxin_name):
 
-    output=file(fluxin_name, 'w')
-    voxels=sm.iterateHex('xyz')
+def print_fluxes(sm, num_e_groups, backward_bool, fluxin_name):
+    """ """    
+    output = open(fluxin_name, 'w')
+    voxels = sm.iterateHex('xyz')
 
     #Print fluxes for each voxel in xyz order (z changing fastest)
     for voxel in voxels:
         
         #Establish for loop bounds based on if forward or backward printing
         #is requested
-        if backward_bool == False :
+        if backward_bool == False:
             min = 1
-            max= num_e_groups + 1
+            max = num_e_groups + 1
             direction = 1
         else:
-            min=num_e_groups
-            max=0
+            min = num_e_groups
+            max = 0
             direction=-1
         
         #Print flux data to file
         count=0
         for e_group in range(min,max,direction):
-            output.write(str(sm.imesh.getTagHandle('n_group_{0:03d}'.format(e_group))[voxel])+' ')
+            output.write(str(sm.imesh.getTagHandle('n_group_{0:03d}'.format( \
+                    e_group))[voxel]) + ' ')
             
             #flux.in formatting: create a new line after every 8th entry
             count += 1   
-            if count%8 == 0:
+            if count % 8 == 0:
                 output.write('\n')
         output.write('\n\n')
    
@@ -64,15 +67,16 @@ def print_fluxes(sm, num_e_groups, backward_bool, fluxin_name):
 
 
 def write_alara_fluxin( filename, sm, backwards=False ):
-
+    """ """    
     #Find number of energy groups
-    num_e_groups=find_num_e_groups(sm)
+    num_e_groups = find_num_e_groups(sm)
 
     #Print flux.in file
     print_fluxes(sm, num_e_groups, backwards, filename)
 
-def main( arguments = None ):
 
+def main( arguments=None ):
+    """ """    
     #Instatiate options parser
     parser = OptionParser\
              (usage='%prog <structured mesh> [options]')
@@ -87,12 +91,12 @@ def main( arguments = None ):
 
     (opts, args) = parser.parse_args( arguments )
 
-    if len(args) != 2 :
+    if len(args) != 2:
         parser.error\
         ( '\nNeed exactly 1 argument: structured mesh file' )
 
     #Load Structured mesh from file
-    sm=ScdMesh.fromFile(args[1])
+    sm = ScdMesh.fromFile(args[1])
 
     write_alara_fluxin( opts.fluxin_name, sm, opts.backward_bool )
 
