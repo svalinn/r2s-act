4d44b18f02885027954980e54f705a47e966e132
Elliott Biondo <biondo@leviathan.cae.wisc.edu>
2013-05-28 11:46:40 -0500

    refactor of magic.py

--- scripts/tools/magic.py
+++ scripts/tools/magic.py
@@ -9,79 +9,99 @@ from itaps import iMesh
 from itaps import iBase
 # r2s imports
 from r2s.scdmesh import ScdMesh
+from tools import h5m_to_wwinp
 
 
+################################################################################
+def gen_e_group_names(flux_mesh, totals_bool):
 
-def find_max_fluxes(flux_mesh, particle, e_groups, total_bool):
-        
-    max_fluxes = [0]*len(e_groups)
-    for i, e_group in enumerate(e_groups):
+    # find meshtal type
+    particle_int = flux_mesh.imesh.getTagHandle('particle')[flux_mesh.imesh.rootSet]
+
+    if particle_int == 1:
+        particle = 'n' 
+    else:
+        particle = 'p'
+
+    # find number of energy groups
+    if totals_bool == True :
+        e_group_names = ['{0}_group_total'.format(particle)]
+
+    elif totals_bool == False:
+        e_upper_bounds = flux_mesh.imesh.getTagHandle("E_upper_bounds")[flux_mesh.imesh.rootSet]
+
+        if isinstance(e_upper_bounds, float):
+            e_upper_bounds = [e_upper_bounds]
+
+        num_e_groups = len(e_upper_bounds)    
+        e_group_names = ['{0}_group_{1:03d}'.format(particle, x) for x in range(1, num_e_groups + 1)]
+
+
+    return e_group_names
+
+################################################################################
+def find_max_fluxes(flux_mesh, e_group_names):
+
+    max_fluxes = [0]*len(e_group_names)
+    for i, e_group_name in enumerate(e_group_names):
+
         for flux_voxel in flux_mesh.iterateHex('xyz'):
-            flux = flux_mesh.imesh.getTagHandle('{0}_group_{1}'.format(particle, e_group))[flux_voxel]
+            flux = flux_mesh.imesh.getTagHandle(e_group_name)[flux_voxel]
+
             if flux > max_fluxes[i]:
                 max_fluxes[i] = flux
 
-    #print "\tMaximum flux(es) found to be {0}".format(max_fluxes)
+    print max_fluxes
+
     return max_fluxes
 
-def magic_wwinp(flux_mesh, ww_mesh='None', total_bool=False, null_value=0, tolerance=0.1):
-    """This function reads in a flux mesh and a ww mesh as well as relevant paramters
-       then the magic method is applied and a newly tagged flux is returned.
-    """
+################################################################################
+def create_ww_mesh(flux_mesh, e_group_names):
 
-    # find meshtal type
+    ww_mesh = ScdMesh(flux_mesh.getDivisions('x'),\
+                       flux_mesh.getDivisions('y'),\
+                       flux_mesh.getDivisions('z'))
 
-    tag_names = []
-    for tag in flux_mesh.imesh.getAllTags(flux_mesh.getHex(0,0,0)):
-        tag_names.append(tag.name)
+    # create ww tags
+    for e_group_name in e_group_names:
+            ww_tag = ww_mesh.imesh.createTag('ww_{0}'.format(e_group_name), 1, float)
+            for voxel in ww_mesh.iterateHex('xyz'):
+                ww_tag[voxel] = 0
 
-    if 'n_group_001' in tag_names or 'n_group_total' in tag_names:
-       particle = 'n'
-    elif 'p_group_001' in tag_names or 'p_group_total' in tag_names:
-        particle = 'p'
-    else:
-        print >>sys.stderr, 'Tag X_group_YYY or X_group_total not found'
-        sys.exit(1)
+    # create e_upper_bound tags
+    e_upper_bounds = flux_mesh.imesh.getTagHandle("E_upper_bounds")[flux_mesh.imesh.rootSet]
+    if isinstance(e_upper_bounds, float):
+        e_upper_bounds = [e_upper_bounds]
 
-    # find number of e_groups
-    num_e_groups  = len(flux_mesh.imesh.getTagHandle("E_upper_bounds")[ww_mesh.imesh.rootSet])
+    e_tag = ww_mesh.imesh.createTag("E_upper_bounds", len(e_upper_bounds), float)
+    e_tag[ww_mesh.imesh.rootSet] = flux_mesh.imesh.getTagHandle("E_upper_bounds")[flux_mesh.imesh.rootSet]
 
-    if total_bool == False:
-        e_groups = ['{0:03d}'.format(x) for x in range(1, num_e_groups + 1)]
-        print "\tGenerating WW for {0} energy groups".format(num_e_groups)
-    else:
-        e_groups = ['total']
-        print "\tGenerating WW for Total energy group"
+    # create  particle tag
+    particle = flux_mesh.imesh.getTagHandle("particle")[flux_mesh.imesh.rootSet]
+    particle_tag = ww_mesh.imesh.createTag("particle", 1, int)
+    particle_tag[ww_mesh.imesh.rootSet] = particle
 
-    # find the max flux value for each e_group, store in vector
-    max_fluxes = find_max_fluxes(flux_mesh, particle, e_groups, total_bool)
+    return ww_mesh
 
-    if ww_mesh == 'None':
+################################################################################
+
+def magic(flux_mesh, totals_bool, null_value, tolerance, ww_mesh=None):
+
+    e_group_names = gen_e_group_names(flux_mesh, totals_bool)
+
+    max_fluxes = find_max_fluxes(flux_mesh, e_group_names)
+    
+    if ww_mesh == None:
         print "\tNo WW mesh file supplied; generating one based on meshtal"
         ww_bool = False # mesh file NOT preexisting
+
         # create a mesh with the same dimensions as flux_mesh
-        ww_mesh = ScdMesh(flux_mesh.getDivisions('x'),\
-                          flux_mesh.getDivisions('y'),\
-                          flux_mesh.getDivisions('z'))
-        # create a tag for each energy group
-        for e_group in e_groups:
-            group_name = "ww_{0}_group_{1}".format(particle, e_group)
-            ww_mesh.imesh.createTag(group_name, 1, float)   
-
-        # create energy bounds
-        tag_e_groups = ww_mesh.imesh.createTag("e_groups", len(e_groups), float)
-
-        if e_groups != ['total']:
-            tag_e_groups[ww_mesh.imesh.rootSet] = \
-                flux_mesh.imesh.getTagHandle("e_groups")[flux_mesh.imesh.rootSet]
-        else:
-            tag_e_groups[ww_mesh.imesh.rootSet] = 1E36 # usual MCNP value           
-
+        ww_mesh = create_ww_mesh(flux_mesh, e_group_names)
 
     else:
         ww_bool = True # mesh file preexisting
+
         # make sure the supplied meshes have the same dimenstions
-        ww_mesh = ScdMesh.fromFile(ww_mesh)
         try:
             for i in ('x', 'y', 'z'):
                 flux_mesh.getDivisions(i) == ww_mesh.getDivisions(i)
@@ -90,58 +110,57 @@ def magic_wwinp(flux_mesh, ww_mesh='None', total_bool=False, null_value=0, toler
             print >>sys.stderr, 'Mismatched dimensions on WWINP and flux meshes'
             sys.exit(1)
 
-    print "\tSupplied meshes confirmed to have same dimensions"
-    
-    # iterate through all voxels          
+        print "\tSupplied meshes confirmed to have same dimensions"
+
+
+    # iterate through all voxels and energy groups and apply MAGIC  
     flux_voxels = flux_mesh.iterateHex('xyz')
     ww_voxels = ww_mesh.iterateHex('xyz')
 
     for (flux_voxel, ww_voxel) in zip(flux_voxels, ww_voxels):
-        for i, e_group in enumerate(e_groups):
-            flux = flux_mesh.imesh.getTagHandle(\
-                '{0}_group_{1}'.format(particle, e_group))[flux_voxel]
-            error = flux_mesh.imesh.getTagHandle(\
-                 '{0}_group_{1}_error'.format(particle, e_group))[flux_voxel]
-            if ((ww_bool == False and error != 0.0) \
-            or (0.0 < error and error < tolerance)):
-                if ww_bool == True:
-                    if ww_mesh.imesh.getTagHandle('ww_{0}_group_{1}'\
-                    .format(particle, e_group))[ww_voxel] != -1:       
-                        ww_mesh.imesh.getTagHandle('ww_{0}_group_{1}'\
-                        .format(particle, e_group))[ww_voxel]\
-                        = flux/(2*max_fluxes[i]) # apply magic method
 
-                else:
-                    ww_mesh.imesh.getTagHandle(\
-                        'ww_{0}_group_{1}'.format(particle, e_group))[ww_voxel]\
-                         = flux/(2*max_fluxes[i]) # apply magic method
+        for i, e_group_name in enumerate(e_group_names):
+
+            flux = flux_mesh.imesh.getTagHandle(e_group_name)[flux_voxel]
+            error = flux_mesh.imesh.getTagHandle(e_group_name)[flux_voxel]
+            ww = ww_mesh.imesh.getTagHandle('ww_{0}'.format(e_group_name))[ww_voxel]
+
+            if (ww_bool == False and error != 0.0) or \
+               (0.0 < error and error < tolerance and ww != -1):
+
+                ww_mesh.imesh.getTagHandle('ww_{0}'.format(e_group_name))[ww_voxel]  = flux/(2*max_fluxes[i]) # apply magic method
 
             elif ww_bool == False and error == 0.0 :
-                ww_mesh.imesh.getTagHandle(\
-                    'ww_{0}_group_{1}'.format(particle, e_group))[ww_voxel]\
-                     = null_value
+                ww_mesh.imesh.getTagHandle('ww_{0}'.format(e_group_name))[ww_voxel] = null_value
 
-    return ww_mesh, e_groups
+    return ww_mesh
 
 
+################################################################################
 
-
-
-
-
-def magic(flux_h5m, ww_mesh, total_bool, null_value, output, output_mesh, tolerance):
-    """Runs magic.py from as a module
+def write_magic(flux_mesh_filename, ww_inp_mesh_filename, totals_bool, null_value, output, output_mesh, tolerance):
+    """Opens up h5m files, sends them to the magic function, saves resulting WW
+       mesh files and converts to a wwinp
     """
-    flux_mesh = ScdMesh.fromFile(flux_h5m)
 
-    ww_mesh, e_groups = magic_wwinp(flux_mesh, ww_mesh, total_bool, null_value, tolerance)
+    flux_mesh = ScdMesh.fromFile(flux_mesh_filename)
 
-    if output_mesh != 'None':
-        ww_mesh.scdset.save(output_mesh)
+    if ww_inp_mesh_filename != None:
+        ww_inp_mesh = ScdMesh.fromFile(ww_inp_mesh_filename)
 
-    write_wwinp(ww_mesh, e_groups, output)
+    else:
+        ww_inp_mesh = None
+   
+
+    ww_mesh = magic(flux_mesh, totals_bool, null_value, tolerance, ww_inp_mesh)
+    ww_mesh.scdset.save(output_mesh)
+    print "\tWrote WW mesh file '{0}'".format(output_mesh)
+
+    #h5m_to_wwinp.write_wwinp(ww_mesh, output)
+    #print "\tWrote WWINP file '{0}'".format(output)
 
 
+################################################################################
 
 def main( arguments = None ):
 
@@ -149,25 +168,26 @@ def main( arguments = None ):
     parser = OptionParser\
              (usage='%prog <flux mesh> <ww mesh> [options]')
 
-    parser.add_option('-w', dest='ww_mesh', default='None',\
+    parser.add_option('-w', dest='ww_mesh', default=None,\
         help='Preexisting WW mesh to apply magic to, default=%default')
 
     parser.add_option('-o', dest='output_name', default='wwinp.out',\
         help='Name of WWINP output file, default=%default')
 
-    parser.add_option('-m', dest='output_mesh', default='None',\
+    parser.add_option('-m', dest='output_mesh', default='magic_ww.h5m',\
         help='Name of WWINP output file, default=%default')
 
-    parser.add_option('-t', action='store_true', dest='total_bool',\
+    parser.add_option('-t', action='store_true', dest='totals_bool',\
         default=False, \
         help='If multiple energy groups exist, only use Total \
          default=%default')
 
     parser.add_option('-n', dest='null_value', default='0',\
-        help='WW value for voxels with error > 10%, default=%default')
+        help='WW value for voxels with error > tolerance, default=%default')
 
     parser.add_option('-e', dest='tolerance', default='0.1',\
-        help='Specify the maximum allowable error for overwriting  values, default=%default')
+        help='Specify the maximum allowable relative error for \
+              overwriting existing ww values, default=%default')
 
     (opts, args) = parser.parse_args( arguments )
 
@@ -176,14 +196,8 @@ def main( arguments = None ):
         ( '\nNeed exactly 1 argument: flux mesh' )
 
 
-    magic(args[0], opts.ww_mesh, opts.total_bool, opts.null_value, opts.output_name, opts.output_mesh, opts.tolerance)
+    write_magic(args[0], opts.ww_mesh, opts.totals_bool, opts.null_value, opts.output_name, opts.output_mesh, opts.tolerance)
 
-    print "\tWrote WWINP file '{0}'".format(opts.output_name)
-
-    if opts.output_mesh != 'None':
-        print "\tWrote WW mesh file '{0}'".format(opts.output_mesh)
-
-    print "Complete"
 
 
 if __name__ == '__main__':
